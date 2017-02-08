2b7d7ba162b02f9d04f0c063750745cebf8ca13a
brisanocturna <ramon.soler.hernan@gmail.com>
2017-02-08 11:01:47 +0100

    modificacion boton
    modificado el boton para testear

--- WindowsFormsApplication9/Form1.Designer.cs
+++ WindowsFormsApplication9/Form1.Designer.cs
@@ -33,11 +33,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(108, 51);
+            this.button1.Location = new System.Drawing.Point(30, 22);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(164, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "boton absurdo";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
